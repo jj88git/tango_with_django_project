c35d73555a98331550f1a038f01293b0d805643f
Jason <jasonxu120506@gmail.com>
2026-01-29 15:22:03 +0000

    completed exercise and Passed Chapter 7 test

--- rango/views.py
+++ rango/views.py
@@ -5,7 +5,9 @@ from rango.models import Page
 from rango.models import Category
 
 from rango.forms import CategoryForm
+from rango.forms import PageForm
 from django.shortcuts import redirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -52,4 +54,33 @@ def add_category(request):
         else:
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
-            
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+    
+    if category is None:
+        return redirect('/rango/')
+    
+    form = PageForm()
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+
+                return redirect(reverse('rango:show_category',
+                                        kwargs={'category_name_slug':
+                                                category_name_slug}))
+        else:
+            print(form.errors)
+
+    context_dict = {'form': form, 'category': category}
+    return render(request, 'rango/add_page.html', context = context_dict)
